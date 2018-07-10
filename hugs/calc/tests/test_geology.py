7aa18f0f41bf7c0151478c349b851344992040da
aspenyoo <blobface@Aspens-MacBook-Pro.local>
2018-07-10 10:04:34 -0500

    add more tests

--- hugs/calc/tests/test_geology.py
+++ hugs/calc/tests/test_geology.py
@@ -1,11 +1,16 @@
 """Test the `geology` module."""
 
 from hugs.calc import snell_angle
-
 from numpy.testing import assert_almost_equal
-
+import pytest
 
 def test_snell():
     """Test the basic wind component calculation."""
     res = snell_angle(12, 2500, 4000)
     assert_almost_equal(res, 19.43022, 4)
+
+
+def test_top_velocity_error():
+    """Test that warning is raised when velocity of top error is 0."""
+    with pytest.raises(ValueError):
+        snell_angle(4.,0.,0.)
--- hugs/calc/tests/test_met.py
+++ hugs/calc/tests/test_met.py
@@ -1,10 +1,9 @@
 """Test the `met` module."""
 
-from hugs.calc import get_wind_dir, get_wind_speed
-
+from hugs.calc import get_wind_dir, get_wind_speed, get_wind_components
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
-
+import pytest
 
 def test_speed():
     """Test calculating wind speed."""
@@ -35,3 +34,28 @@ def test_dir():
     true_dir = np.array([270., 225., 180., 270.])
 
     assert_array_almost_equal(true_dir, direc, 4)
+
+def test_wind_components_scalar():
+    """Test wind_components with scalar inputs."""
+    u,v = get_wind_components(4., 2.)
+
+    assert_array_almost_equal(u, -0.1395, 4)
+    assert_array_almost_equal(v, -3.9976, 4)
+
+def test_wind_components():
+    """Test wind_components with array inputs."""
+    speed = np.array([4., 2., 5., 0.])
+    wdir = np.array([0.,300., 40., 135.])
+
+    idx = 2
+    u,v = get_wind_components(speed, wdir)
+    uu,vv = get_wind_components(speed[idx],wdir[idx])
+    assert(len(speed) == len(u))
+    assert(len(speed) == len(v))
+    assert_array_almost_equal(u[idx], uu, 4)
+    assert_array_almost_equal(v[idx], vv, 4)
+
+def test_warning_direction():
+    """Test that warning is raised when wind direction > 360."""
+    with pytest.warns(UserWarning):
+        get_wind_components(4.,554.)
