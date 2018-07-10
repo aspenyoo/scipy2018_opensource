c33caa4488978ab529998b6d811e90b0d882b58e
aspenyoo <blobface@Aspens-MacBook-Pro.local>
2018-07-10 11:21:06 -0500

    add flake8 to travis

--- hugs/calc/tests/test_met.py
+++ hugs/calc/tests/test_met.py
@@ -1,14 +1,18 @@
 """Test the `met` module."""
 
 from hugs.calc import get_wind_dir, get_wind_speed, get_wind_components
+
 import numpy as np
+
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
+
 import pytest
+
 
 def test_speed():
     """Test calculating wind speed."""
-    u = np.array([4., 2.,0., 0.])
-    v = np.array([0.,2., 4., 0.])
+    u = np.array([4., 2., 0., 0.])
+    v = np.array([0., 2., 4., 0.])
 
     speed = get_wind_speed(u, v)
 
@@ -35,27 +39,30 @@ def test_dir():
 
     assert_array_almost_equal(true_dir, direc, 4)
 
+
 def test_wind_components_scalar():
     """Test wind_components with scalar inputs."""
-    u,v = get_wind_components(4., 2.)
+    u, v = get_wind_components(4., 2.)
 
     assert_array_almost_equal(u, -0.1395, 4)
     assert_array_almost_equal(v, -3.9976, 4)
 
+
 def test_wind_components():
     """Test wind_components with array inputs."""
     speed = np.array([4., 2., 5., 0.])
-    wdir = np.array([0.,300., 40., 135.])
+    wdir = np.array([0., 300., 40., 135.])
 
     idx = 2
-    u,v = get_wind_components(speed, wdir)
-    uu,vv = get_wind_components(speed[idx],wdir[idx])
+    u, v = get_wind_components(speed, wdir)
+    uu, vv = get_wind_components(speed[idx], wdir[idx])
     assert(len(speed) == len(u))
     assert(len(speed) == len(v))
     assert_array_almost_equal(u[idx], uu, 4)
     assert_array_almost_equal(v[idx], vv, 4)
 
+
 def test_warning_direction():
     """Test that warning is raised when wind direction > 360."""
     with pytest.warns(UserWarning):
-        get_wind_components(4.,554.)
+        get_wind_components(4., 554.)
